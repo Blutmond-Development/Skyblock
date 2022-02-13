273fc6806cfd5270182c8311847eab9ea7a170c1
Christian Raoulis <craoulis@googlemail.com>
2022-02-13 19:54:46 +0100

    add tooltip information about minion in PickedMinionItem

--- src/main/java/de/blutmondgilde/skyblock/item/PickedMinionItem.java
+++ src/main/java/de/blutmondgilde/skyblock/item/PickedMinionItem.java
@@ -3,19 +3,25 @@ package de.blutmondgilde.skyblock.item;
 import de.blutmondgilde.skyblock.entity.minion.MinionEntity;
 import de.blutmondgilde.skyblock.registry.SkyblockRegistries;
 import de.blutmondgilde.skyblock.registry.SkyblockTabs;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 public class PickedMinionItem extends Item {
     public PickedMinionItem() {
@@ -34,8 +40,7 @@ public class PickedMinionItem extends Item {
 
     private boolean placeMinion(Player player, BlockPos position, Direction facing, Level world, ItemStack stack) {
         if (world.isClientSide()) return false;
-        if (!stack.hasTag()) return false;
-        if (!stack.getTag().contains("minion")) return false;
+        if (!hasMinionTag(stack)) return false;
         MinionEntity minionEntity = getEntityFromStack(stack, world);
         if (minionEntity == null) return false;
         position = position.relative(facing);
@@ -73,5 +78,21 @@ public class PickedMinionItem extends Item {
             }
         }
         return null;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        if (hasMinionTag(pStack)) {
+            CompoundTag tag = pStack.getTag();
+            pTooltipComponents.add(new TranslatableComponent("item.skyblock.picked_minion.tooltip.type", new TranslatableComponent(tag.getString("minion").replace(':', '.'))));
+            pTooltipComponents.add(new TranslatableComponent("item.skyblock.picked_minion.tooltip.level", tag.getInt("level")));
+        } else {
+            pTooltipComponents.add(new TranslatableComponent("item.skyblock.picked_minion.tooltip.notag").withStyle(Style.EMPTY.withColor(ChatFormatting.RED).withBold(true)));
+        }
+    }
+
+    private boolean hasMinionTag(ItemStack itemStack) {
+        if (!itemStack.hasTag()) return false;
+        return itemStack.getTag().contains("minion");
     }
 }
